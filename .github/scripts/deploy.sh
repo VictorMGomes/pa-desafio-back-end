4743fd0740a0bbc488731e5a6ada1f430b7d25ae
Victor Gomes <victorvhmgomes@gmail.com>
2024-06-17 10:19:48 -0300

    Test Change Directory

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,16 +2,4 @@
 
 cd $APP_PATH
 
-git pull origin main
-
-omposer install --no-dev --no-progress --prefer-dist
-
-if [ -n $PRODUCTION_ENV ]; then
-  cp $PRODUCTION_ENV .env
-fi
-
-php artisan migrate --force
-
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+touch test.txt
