ac8205ffb0c26190d7c9485338eb81288ea0d66d
Victor Gomes <victorvhmgomes@gmail.com>
2024-06-17 09:06:57 -0300

    Fix deploy action

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
 
-set -e
+cd "$APP_PATH" || exit
 
-# Define the application path from GitHub Secrets
-APP_PATH=${APP_PATH}
-
-# Navigate to the application path
-cd $APP_PATH
-
-# Pull the latest changes from the repository
 git pull origin main
 
-# Install/update Composer dependencies
-composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-dev --no-progress --prefer-dist
 
-# Copy production .ENV file
-cp ${PRODUCTION_ENV} ${APP_PATH}
+if [ -n "$PRODUCTION_ENV" ]; then
+  cp "$PRODUCTION_ENV" .env
+fi
 
-# Run database migrations
 php artisan migrate --force
 
-# Clear and cache the configuration
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Set correct permissions (adjust as necessary)
-chown -R www-data:www-data $APP_PATH
-chmod -R 775 $APP_PATH/storage
-chmod -R 775 $APP_PATH/bootstrap/cache
-
-# Restart the server or queue workers if necessary
-# sudo systemctl restart apache2
-# sudo systemctl restart php-fpm
