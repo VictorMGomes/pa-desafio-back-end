6266ccfa1a99b0ec445b50254fe668afbca4e611
Victor Gomes <victorvhmgomes@gmail.com>
2024-06-24 04:34:15 -0300

    Add posts routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,9 @@ Route::get('/users/{ID}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{ID}', [UserController::class, 'update']);
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{ID}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::put('/posts/{ID}', [PostController::class, 'update']);
+Route::delete('/posts/{ID}', [PostController::class, 'destroy']);
